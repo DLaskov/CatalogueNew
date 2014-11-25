179051dd2483783f782934f12b384834b97d467f
DeathWinged <mitkobfb2@gmail.com>
2014-11-25 14:00:52 +0200

    commit

---
+++ CatalogueNew/CatalogueNew.Models/Services/ICategoryService.cs
@@ -0,0 +1,20 @@
+﻿using CatalogueNew.Models.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace CatalogueNew.Models.Services
+{
+    public interface ICategoryService
+    {
+        IEnumerable<Category> GetAll();
+
+        Category Find(int id);
+
+        void Add(Category category);
+
+        void Modify(Category category);
+
+        void Remove(Category category);
+
+    }
+}
