82067711817f830d73ffba4e11e0b92c81ab8eee
DeathWinged <mitkobfb2@gmail.com>
2014-12-01 16:42:02 +0200

    Manufacturers

--- CatalogueNew/CatalogueNew/Controllers/ManufacturerController.cs
+++ CatalogueNew/CatalogueNew/Controllers/ManufacturerController.cs
@@ -1,7 +1,10 @@
-﻿using CatalogueNew.Models.Services;
+﻿using CatalogueNew.Models.Entities;
+using CatalogueNew.Models.Services;
+using CatalogueNew.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,17 +12,148 @@ namespace CatalogueNew.Web.Controllers
 {
     public class ManufacturerController : Controller
     {
-        private IManufacturerService manufacturerService;
+        private IManufacturerService manufacturerServices;
 
-        // GET: Manufacturer
-        public ActionResult Index()
+        public ManufacturerController(IManufacturerService manufacturerServices)
+        {
+            this.manufacturerServices = manufacturerServices;
+        }
+
+        public ActionResult Index(int? page)
+        {
+            var pageItems = manufacturerServices.GetItems(page);
+
+            var categoryListViewModel = new ManufacturerListViewModel()
+            {
+                Manufacturers = pageItems.Items.ToList(),
+                Count = pageItems.PageCount,
+                Page = pageItems.CurrentPage
+            };
+
+            return View(categoryListViewModel);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Manufacturer manufacturer = manufacturerServices.Find(id);
+
+            if (manufacturer == null)
+            {
+                return HttpNotFound();
+            }
+
+            var model = new ManufacturerViewModel()
+            {
+                ManufacturerID = manufacturer.ManufacturerID,
+                Name = manufacturer.Name,
+                Description = manufacturer.Description
+            };
+
+            return View(model);
+        }
+
+        public ActionResult Create()
         {
             return View();
         }
 
-        public ManufacturerController(IManufacturerService manufacturerService)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ManufacturerViewModel model)
         {
-            this.manufacturerService = manufacturerService;
+            if (ModelState.IsValid)
+            {
+                var manufacturer = new Manufacturer()
+                {
+                    Name = model.Name,
+                    Description = model.Description
+                };
+
+                manufacturerServices.Add(manufacturer);
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Manufacturer manufacturer = manufacturerServices.Find(id);
+
+            if (manufacturer == null)
+            {
+                return HttpNotFound();
+            }
+
+            var model = new ManufacturerViewModel()
+            {
+                ManufacturerID = manufacturer.ManufacturerID,
+                Name = manufacturer.Name,
+                Description = manufacturer.Description
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ManufacturerViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var manufacturer = new Manufacturer()
+                {
+                    ManufacturerID = model.ManufacturerID,
+                    Name = model.Name,
+                    Description = model.Description
+                };
+                manufacturerServices.Modify(manufacturer);
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Manufacturer manufacturer = manufacturerServices.Find(id);
+
+            if (manufacturer == null)
+            {
+                return HttpNotFound();
+            }
+
+            var model = new ManufacturerViewModel()
+            {
+                ManufacturerID = manufacturer.ManufacturerID,
+                Name = manufacturer.Name,
+                Description = manufacturer.Description
+            };
+
+            return View(model);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            manufacturerServices.Remove(id);
+            return RedirectToAction("Index");
         }
 
 
--- CatalogueNew/CatalogueNew/Models/ManufacturerListViewModel.cs
+++ CatalogueNew/CatalogueNew/Models/ManufacturerListViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CatalogueNew.Models.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,10 @@ namespace CatalogueNew.Web.Models
 {
     public class ManufacturerListViewModel
     {
+        public int ManufacturerID { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Manufacturer> Manufacturers { get; set; }
+        public int Count { get; set; }
+        public int? Page { get; set; }
     }
 }
