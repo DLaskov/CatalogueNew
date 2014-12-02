955e6fc9555340c9b7e2e6da443b085f46551c4f
Nikola Nikolov <nikolov_nikola@outlook.com>
2014-12-02 15:17:59 +0200

    ImageHandler

---
+++ CatalogueNew/CatalogueNew/Controllers/ImageController.cs
@@ -0,0 +1,32 @@
+﻿using CatalogueNew.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CatalogueNew.Web.Controllers
+{
+    public class ImageController : Controller
+    {
+        private readonly ICatalogueContext context;
+
+        public ImageController(ICatalogueContext context)
+        {
+            this.context = context;
+        }
+
+        //[HttpPost]
+        //public ActionResult Upload()
+        //{
+        //    Image image = new Image
+        //    {
+        //        ImageName = "Test image",
+        //        MimeType = "image/jpeg",
+        //        UpdatedAt = DateTime.Now,
+
+        //    };
+        //}
+    }
+}
