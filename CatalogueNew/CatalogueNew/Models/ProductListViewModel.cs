eb0e3ea6d73470143d1d0f050b7d4e1cc36f155f
Nikola Nikolov <nikolov_nikola@outlook.com>
2014-12-12 11:41:19 +0200

    Product functionality added

---
+++ CatalogueNew/CatalogueNew/Models/ProductListViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CatalogueNew.Models.Entities;
+using CatalogueNew.Models.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CatalogueNew.Web.Models
+{
+    public class ProductListViewModel
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+        public int Count { get; set; }
+        public int Page { get; set; }
+
+        public ProductListViewModel(PagedList<Product> pageItems)
+        {
+            Products = pageItems.Items.ToList();
+            Count = pageItems.PageCount;
+            Page = pageItems.CurrentPage;
+        }
+
+        public ProductListViewModel()
+        {
+
+        }
+    }
+}
