c21a89e668fd920f5935300034ab17ea152877b1
Svetoslav Georgiev <svetoslav.g@mail.com>
2014-12-19 14:51:49 +0200

    comments

---
+++ CatalogueNew/CatalogueNew/Controllers/CommentsController.cs
@@ -0,0 +1,68 @@
+﻿using CatalogueNew.Models.Entities;
+using CatalogueNew.Models.Services;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace CatalogueNew.Web.Controllers
+{
+    public class CommentsController : ApiController
+    {
+        private ICommentService commentsService;
+
+        public CommentsController()
+        {
+        }
+
+        public CommentsController(ICommentService commentsService)
+        {
+            this.commentsService = commentsService;
+        }
+
+        public IEnumerable<Comment> GetByProduct(int productId)
+        {
+            return commentsService.GetByProduct(productId);
+        }
+
+        public IEnumerable<Comment> GetByParent(int parentId)
+        {
+            return commentsService.GetByParent(parentId);
+        }
+
+        public Comment Get(int id)
+        {
+            return commentsService.GetComment(id);
+        }
+
+        public Comment Post([FromBody]Comment value)
+        {
+            var comment = new Comment()
+            {
+                Text = value.Text,
+                ProductID = value.ProductID,
+                UserID = value.UserID,
+                TimeStamp = DateTime.UtcNow
+            };
+
+            commentsService.Add(comment);
+
+            return comment;
+        }
+
+        public void Put([FromBody]Comment comment)
+        {
+            commentsService.Modify(comment);
+        }
+
+        public void Delete([FromBody]Comment comment)
+        {
+            commentsService.Remove(comment);
+        }
+    }
+}
