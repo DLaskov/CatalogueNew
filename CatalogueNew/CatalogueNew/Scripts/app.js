526dde6e54253a1fec477eb22974e5f4e0144701
DeathWinged <mitkobfb2@gmail.com>
2015-01-15 15:42:05 +0200

    Mazno menu!

--- CatalogueNew/CatalogueNew/Scripts/app.js
+++ CatalogueNew/CatalogueNew/Scripts/app.js
@@ -90,6 +90,52 @@
                     "' value='" + data.UniqueName + "\\" + data.ImgName + "\\" + data.MimeType + "' />"
                 $("div .form-horizontal").prepend(innerHtml);
             });
-            }
-            }
-        });
+        }
+    }
+});
+
+$(function () {
+    $('.navbar-brand').data('size', 'big');
+});
+
+$(window).scroll(function () {
+    if ($(document).scrollTop() > 0) {
+        if ($('.navbar-brand').data('size') == 'big') {
+            $('.navbar-brand').data('size', 'small');
+            $('.navbar-brand').stop().animate({
+                height: '0px',
+                padding: '0'
+            }, 600);
+            $('.navbar').stop().animate({
+                height: '50px'
+            }, 600);
+            $('.navbar ul').stop().animate({
+                marginTop: '0'
+            }, 600);
+            $('.navbar-right').stop().animate({
+                marginTop: '0'
+            }, 600);
+            $('.dropdown-menu ul').removeAttr('style');
+        }
+    }
+    else {
+        if ($('.navbar-brand').data('size') == 'small') {
+            $('.navbar-brand').data('size', 'big');
+            $('.navbar-brand').stop().animate({
+                height: '72px'
+            }, 600);
+            $('.navbar').stop().animate({
+                height: '150px'
+            }, 600);
+            $('.navbar ul').stop().animate({
+                marginTop: '7%'
+            }, 600);
+            $('.navbar-right').stop().animate({
+                marginTop: '6%'
+            }, 600);
+            $('.dropdown-menu').stop().animate({
+                marginTop: '0'
+            }, 0);
+        }
+    }
+});
