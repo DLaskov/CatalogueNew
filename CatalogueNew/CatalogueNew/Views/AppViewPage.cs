3bc87d5bebc5d5ea775a99e83d72b53854a4c0f7
DeathWinged <mitkobfb2@gmail.com>
2015-01-21 10:33:30 +0200

    appviewpage

--- CatalogueNew/CatalogueNew/Views/AppViewPage.cs
+++ CatalogueNew/CatalogueNew/Views/AppViewPage.cs
@@ -10,6 +10,16 @@ namespace CatalogueNew.Web.Views
 {
     public abstract class AppViewPage<LogInViewModel> : WebViewPage<LogInViewModel>
     {
-        
+        protected AppUserClaims CurrentUser
+        {
+            get
+            {
+                return new AppUserClaims(this.User as ClaimsPrincipal);
+            }
+        }
+    }
+
+    public abstract class AppViewPage : AppViewPage<dynamic>
+    {
     }
 }
